e3a290930ef03da1b233bda091a1934eab9302ef
Nagumx <166014524+Nagumx@users.noreply.github.com>
2024-04-08 22:12:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('puro cetis')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('puro cobacho')
+});
